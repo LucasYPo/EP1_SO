5af09c4ce83878aad25074c1db8d03baa47867be
Lucas Yudi Po <lucas-yudi.po@outlook.com>
2022-10-11 15:54:54 -0300

    Cria os BCPs e passa as prioridades e cria o arquivo Log

--- src/Escalonador.java
+++ src/Escalonador.java
@@ -1,44 +1,80 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
-import java.lang.Integer;
+import java.util.ArrayList;
 
 public class Escalonador {
-    public static void main(String[] args) {
-        
-	// Iniciliza todos os processos na memoria e os coloca na tabela de 'prontos'.
-	for(int i = 1; i <= 10; i++) {
-		String path = "../programas/";
+	public static void main(String[] args) {
+		ArrayList<BCP> bcpTabela = new ArrayList<>();
+		int quantum = 0;
 
-		if(i < 10) {
-			path += "0" + i + ".txt";
-		}else {
-			path += i + ".txt";
-		}
-
-		File arq = new File(path);
-		File arqPrio = new File("../programas/prioridades.txt");
-		
-		// Lê o conteudo do arquivo e o associa a uma nova instancia de BCP.
-		try {
-			Scanner sc = new Scanner(arq);
-			Scanner scPrio = new Scanner(arqPrio);
-
-			int count = 0;
-			while(sc.hasNextLine() && scPrio.hasNextLine()) {
-				String data = sc.nextLine();
-
-				// Caso seja a primeira linha, cria a instancia de BCP e já atribui sua prioridade.
-				if(count == 0) {
-					int prioridade = Integer.parseInt(scPrio.nextLine());
-					BCP processo = new BCP(data, prioridade);
-				}
-				count++;	
+		for(int i = 1; i < 11; i++) {
+			String path = "../programas/";
+			
+			if(i < 10) {
+				path += "0" + i + ".txt";
+			} else {
+				path += i + ".txt";
 			}
 
-		}catch(FileNotFoundException e) {
-			System.out.println("Não foi possível ler o arquivo.");
+			File arq = new File(path);
+			File arqPrio = new File("../programas/prioridades.txt");
+
+			try {
+				Scanner ltr = new Scanner(arq);
+				Scanner ltrPrio = new Scanner(arqPrio);
+
+				int linha = 0;
+				while(ltr.hasNextLine()) {
+					String data = ltr.nextLine();
+
+					if(linha == 0) {
+						for(int k = 1; k < i; k++) {
+							ltrPrio.nextLine();
+						}
+
+						int valPrio = Integer.parseInt(ltrPrio.nextLine());
+
+						BCP processo = new BCP(data, valPrio);
+
+						bcpTabela.add(processo);
+					}
+
+					linha++;
+				}
+			} catch(FileNotFoundException e) {
+				System.out.println("Não foi possível ler o arquivo " + i );
+				e.printStackTrace();
+			}
+		}
+		
+		File arqQt = new File("../programas/quantum.txt");
+
+		try {
+			Scanner ltrQt = new Scanner(arqQt);
+			quantum = Integer.parseInt(ltrQt.nextLine());
+		
+		} catch(FileNotFoundException e) {
+			System.out.println("Não foi possível ler o arquivo quantum.");
+			e.printStackTrace();
+		}
+
+		String pathLog = "../programas/";
+
+		if(quantum < 10) {
+			pathLog += "Log0" + quantum + ".txt";
+		} else {
+			pathLog += "Log" + quantum + ".txt";
+		}
+
+		File arqLog = new File(pathLog);
+
+		try {
+			arqLog.createNewFile();
+		} catch(IOException e) {
+			System.out.println("Não foi possível criar o arquivo LogXX");
+			e.printStackTrace();
 		}
 	}
-    }
 }
